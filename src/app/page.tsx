1ea9da2c440351bba9c0003e07ffa67b0d6c0826
Minatallah Hisham <mennahishamh@gmail.com>
2023-09-18 16:36:24 +0300

    data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Link from 'next/link';
+type Repository = {
+  id: number;
+  name: string;
+  full_name: string;
+};
 
-export default function Page() {
-  return (
-    <div>
-      <Link href='/about'>About</Link>
-      <Link href='/blog/123'>Blog Post</Link>
-    </div>
-  )
+
+export default async function Page() {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js');
+  const data: Repository = await res.json();
+  return (<h1>{data.full_name}</h1>);
 
 }
